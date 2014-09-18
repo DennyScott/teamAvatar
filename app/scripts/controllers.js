9bb6ed7b6dbb8f90bc27d0a87b3b9c1a2cb291be
Denny Scott <dennyscott301@gmail.com>
2014-09-18 10:41:54 -0500

    add

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -61,6 +61,33 @@ angular.module('FishingHole.controllers', [])
 				}
     });
 
+		 // Triggered on a button click, or some other target
+ $scope.showActions = function() {
+
+   // Show the action sheet
+   var hideSheet = $ionicActionSheet.show({
+     buttons: [
+       { text: '<b>Share</b> This' },
+       { text: 'Move' }
+     ],
+     destructiveText: 'Delete',
+     titleText: 'Modify your album',
+     cancelText: 'Cancel',
+     cancel: function() {
+          // add cancel code..
+        },
+     buttonClicked: function(index) {
+       return true;
+     }
+   });
+
+   // For example's sake, hide the sheet after two seconds
+   $timeout(function() {
+     hideSheet();
+   }, 2000);
+
+ };
+
 		$ionicModal.fromTemplateUrl('templates/addEvent.html', {
     scope: $scope
   }).then(function(modal) {
